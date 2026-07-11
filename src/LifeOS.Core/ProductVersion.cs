dd14c80b550f27dd1224f931ed60004528a42bf9
Codie Shannon <codie.shannon.work@outlook.com>
2026-07-12 00:42:33 +1200

    chore: advance v6 alpha version state

--- src/LifeOS.Core/ProductVersion.cs
+++ src/LifeOS.Core/ProductVersion.cs
@@ -2,7 +2,7 @@ namespace LifeOS.Core;
 
 public static class ProductVersion
 {
-    public const string Semantic = "5.0.0-beta.1";
-    public const string Display = "v5.0.0-beta.1";
-    public const string ReleaseName = "v5 integration release checkpoint";
+    public const string Semantic = "6.0.0-alpha.1";
+    public const string Display = "v6.0.0-alpha.1";
+    public const string ReleaseName = "Controlled automation foundation";
 }
